54ee533541b1c0b7c283a4f6b529591f5bcca58f
Satstef <83057096+Satstef@users.noreply.github.com>
2021-05-01 16:09:35 +0200

    Add files via upload

--- app_SingSong/config.py
+++ app_SingSong/config.py
@@ -1,6 +1,11 @@
 import os
 import tempfile
+from dotenv import load_dotenv
 
+# dotenv carica il file nascosto .env che contiene i valori delle vatiabili ambientali.
+# Comunque da terminale bisogna settare FLASK_APP e FLASK_ENV
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'_5#y2L"F4Q8z\n\xec]/'
@@ -13,8 +18,9 @@ class Config(object):
 
 class Development(Config):
     EXPLAIN_TEMPLATE_LOADING=False
+    # DATABASE_URL è il path alla cartella "DBschema"
     DATABASE = os.path.join(os.environ.get('DATABASE_URL'), 'Database.db')
-    DB_SCHEMA = os.path.join(os.environ.get('DB_SCHEMA_URL'), 'schema.sql')
+    DB_SCHEMA = os.path.join(os.environ.get('DATABASE_URL'), 'schema.sql')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -24,14 +30,14 @@ class Development(Config):
 
 class Testing(Config):
     TESTING = True
-    db_fd, DATABASE = tempfile.mkstemp()
+    DATABASE = tempfile.mkstemp()
 
 
 class Production(Config):
     # Configurare i valori con le variabili d'ambiente. Da terminale lanciare:
     # export MAIL_SERVER = serveremail, ecc.
     DATABASE = os.path.join(os.environ.get('DATABASE_URL'), 'Database.db')
-    DB_SCHEMA = os.path.join(os.environ.get('DB_SCHEMA_URL'), 'schema.sql')
+    DB_SCHEMA = os.path.join(os.environ.get('DATABASE_URL'), 'schema.sql')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
